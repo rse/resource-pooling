4dc6867418ce046f8a09c7aa89a5762e726cef67
Ralf S. Engelschall <rse@engelschall.com>
2018-04-17 23:02:26 +0200

    iniitially place under version control

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# resource-pooling
-Simple Resource Pooling
+
+Resource-Pooling
+=================
+
+Simple Resource Pooling for Node.js
+
+<p/>
+<img src="https://nodei.co/npm/resource-pooling.png?downloads=true&stars=true" alt=""/>
+
+<p/>
+<img src="https://david-dm.org/rse/resource-pooling.png" alt=""/>
+
+Installation
+------------
+
+```shell
+$ npm install resource-pooling
+```
+
+About
+-----
+
+This is a small Node.js library for simple resource pooling. The
+resource can be an arbitrary object and usually is something like a
+database connection.
+
+Application Programming Interface
+---------------------------------
+
+The API of Resource-Pooling is a superset of the `Set` API:
+
+- `constructor(): ResourcePooling`:<br/>
+  Create the [MQTT.js](https://www.npmjs.com/package/mqtt) API wrapper.
+  The `mqtt` is the [MQTT.js](https://www.npmjs.com/package/mqtt) instance.
+  The optional `encoding` can be either `json` (default), `msgpack` or `cbor`.
+
+- `ResourcePooling#register(method: string, callback: (...args: any[]) => any): void`:<br/>
+
+License
+-------
+
+Copyright (c) 2018 Ralf S. Engelschall (http://engelschall.com/)
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
---
+++ resource-pooling.js
@@ -0,0 +1,123 @@
+/*
+**  Resource-Pooling -- Simple Resource Pooling
+**  Copyright (c) 2018 Ralf S. Engelschall <rse@engelschall.com>
+**
+**  Permission is hereby granted, free of charge, to any person obtaining
+**  a copy of this software and associated documentation files (the
+**  "Software"), to deal in the Software without restriction, including
+**  without limitation the rights to use, copy, modify, merge, publish,
+**  distribute, sublicense, and/or sell copies of the Software, and to
+**  permit persons to whom the Software is furnished to do so, subject to
+**  the following conditions:
+**
+**  The above copyright notice and this permission notice shall be included
+**  in all copies or substantial portions of the Software.
+**
+**  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+**  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+**  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+**  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+**  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+**  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+**  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+/*  the API class  */
+class API {
+    constructor () {
+        this.resourcesKnown  = new Set()
+        this.resourcesUnused = new Set()
+    }
+    add (res) {
+        if (this.resourcesKnown.has(res))
+            throw new Error("resource already in pool")
+        this.resourcesKnown.add(res)
+        this.resourcesUnused.add(res)
+    }
+    has (res) {
+        return this.resourcesKnown.has(res)
+    }
+    size () {
+        return this.resourcesKnown.size
+    }
+    remove (res, force = false) {
+        if (!this.resourcesKnown.has(res))
+            throw new Error("resource not in pool")
+        if (!this.resourcesUnused.has(res) && !force)
+            throw new Error("resource still in use")
+        this.resourcesKnown.delete(res)
+        this.resourcesUnused.delete(res)
+    }
+    values () {
+        return this.resourcesKnown.values()
+    }
+    forEach (cb) {
+        this.resourcesKnown.forEach(cb)
+    }
+    clear () {
+        this.resourcesKnown.clear()
+    }
+    acquire (retries = 10, delay = 100) {
+        if (this.resourcesKnown.size === 0)
+            throw new Error("still no resources in pool")
+        return new Promise((resolve, reject) => {
+            const take = () => {
+                if (this.resourcesUnused.size > 0) {
+                    let res = this.resourcesUnused.values().next()
+                    this.resourcesUnused.delete(res.value)
+                    resolve(res.value)
+                }
+                else {
+                    retries--
+                    if (retries < 0)
+                        reject(new Error("failed to aquire resource from pool"))
+                    else
+                        setTimeout(() => { take() }, delay)
+                }
+            }
+            take()
+        })
+    }
+    release (res) {
+        if (!this.resourcesKnown.has(res))
+            throw new Error("resource not known in pool")
+        if (this.resourcesUnused.has(res))
+            throw new Error("resource still not used")
+        this.resourcesUnused.add(res)
+    }
+    used (res) {
+        return !this.resourcesUnused.has(res)
+    }
+    use (cb, retries = 10, delay = 100) {
+        return this.acquire(retries, delay).then(async (res) => {
+            return cb(res).then((result) => {
+                this.release(res)
+                return result
+            }, (err) => {
+                this.release(res)
+                throw err
+            })
+        }, (err) => {
+            throw err
+        })
+    }
+    async drain (cb, retries = 10, delay = 100) {
+        while (this.size() > 0) {
+            await this.acquire(retries, delay).then((res) => {
+                return cb(res).then((result) => {
+                    this.remove(res, true)
+                    return result
+                }, (err) => {
+                    this.release(res)
+                    throw err
+                })
+            }, (err) => {
+                throw err
+            })
+        }
+    }
+}
+
+/*  export the standard way  */
+module.exports = API
+
